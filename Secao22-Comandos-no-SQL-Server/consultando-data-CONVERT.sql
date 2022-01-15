d16e21285664a514200c487762a32dd608769e0a
Adriano Santos <adriradiologista@hotmail.com.br>
2022-01-14 23:11:52 -0300

    Alterando os comentários

--- Secao22-Comandos-no-SQL-Server/consultando-data-CONVERT.sql
+++ Secao22-Comandos-no-SQL-Server/consultando-data-CONVERT.sql
@@ -20,24 +20,4 @@ AMANDA                         09/12/1981
 ADRIANO                        09/03/1976
 RAFAEL                         09/07/1991
 JHONATA                        09/11/2000
-
-<<<<<<< HEAD
 */
-=======
-ANDRE		09/12/1981
-ANA		09/03/1978
-RUI		09/07/1951
-JOAO		09/11/2002
-ANDREIA		09/12/1985
-ANAKARLA	09/03/1979
-ROBERVAL	09/07/1981
-JOANETE		09/10/2001
-AMANDA		09/12/1981
-ADRIANO		09/03/1976
-RAFAEL		09/07/1991
-JHONATA		09/11/2000
-*/
-<<<<<<< HEAD
->>>>>>> 0393c35ae249c1b20b0738ebe0dff61abf9a7fa3
-=======
->>>>>>> 0393c35ae249c1b20b0738ebe0dff61abf9a7fa3
